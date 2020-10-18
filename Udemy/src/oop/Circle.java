eec5a133dc524ba136e0f462eb2c7cbf723ccdde
pujabhatt <pujabhatt@192.168.100.3>
2020-10-18 16:34:28 +0545

    new commit

--- Udemy/src/oop/Circle.java
+++ Udemy/src/oop/Circle.java
@@ -1,42 +1,22 @@
 package oop;
 
 public class Circle {
-
-	double radius;
-
-	public Circle(double radius) {
-		this.radius = radius;
-		if (radius < 0) {
-			this.radius = 0;
-		}
-
-	}
-
-	double getRadius() {
-
-		return this.radius;
-	}
-
-	public double getArea() {
-		return (Math.PI * radius * radius);
-	}
-
-	class Cylinder extends Circle {
-
-		
-
-		public Cylinder(double radius) {
-			super(radius);
-			// TODO Auto-generated constructor stub
-		}
-
-		double height;
-
-		public double Cylinder(double radius, double height) {
-			return height;
-
-		}
-
-	}
-
+    private double radius;
+ 
+    public Circle(double radius) {
+        if (radius < 0) {
+            this.radius = 0;
+        } else {
+            this.radius = radius;
+        }
+    }
+ 
+    public double getRadius() {
+        return radius;
+    }
+ 
+    public double getArea() {
+        return (radius * radius * Math.PI);
+    }
 }
+
